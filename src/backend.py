d7c11c5f7f0470a62903f722aab87cccb4401e92
w1ll-farmer <90869013+w1ll-farmer@users.noreply.github.com>
2025-05-05 17:11:36 +0100

    Compiled dex

--- src/backend.py
+++ src/backend.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import json
 import os
+import requests
 
 global COLS
 COLS = ["Raw count","Abilities","Items","Spreads","Moves","Teammates","Checks and Counters", "usage"]
@@ -14,7 +15,7 @@ COLS = ["Raw count","Abilities","Items","Spreads","Moves","Teammates","Checks an
 # Teammates {teammate:count}
 # Checks and Counters {check/counter:[count appeared on opposite teams, ko/force switch out %, std]}
 
-def get_pokemon_info(pokemon_name=None, format="gen9vgc2025regg-1760"):
+def get_pokemon_smogon_info(pokemon_name=None, format="gen9vgc2025regg-1760"):
     if pokemon_name is None:
         print("Must enter a pokemon name")
         return None
@@ -29,4 +30,85 @@ def get_pokemon_info(pokemon_name=None, format="gen9vgc2025regg-1760"):
     print("Either there's no data about this pokemon or you have entered their name in the wrong format")
     return None
 
-info = get_pokemon_info("Infernape")
+
+def get_pokemon_base_info(pokemon_name):
+    print(pokemon_name)
+    if " " in pokemon_name:
+        space_idx = pokemon_name.index(" ")
+        pokemon_name = pokemon_name[:space_idx]+"-"+pokemon_name[space_idx+1:]
+    if "Ogerpon" in pokemon_name and pokemon_name != "Ogerpon":
+        pokemon_name += "-mask"
+    if pokemon_name in ["Landorus","Tornadus","Thundurus","Enamorus"]:
+        pokemon_name+= "-incarnate"
+    if pokemon_name.endswith("-M"): pokemon_name += "ale"
+    if pokemon_name.endswith("-F"): pokemon_name += "emale"
+    if pokemon_name == "Urshifu": pokemon_name = "urshifu-single-strike"
+    if pokemon_name == "Tatsugiri": pokemon_name = "tatsugiri-curly"
+    if pokemon_name == "Maushold": pokemon_name = "maushold-family-of-three"
+    if pokemon_name == "Basculegion": pokemon_name = "basculegion-male"
+    if pokemon_name == "Meowstic": pokemon_name = "meowstic-male"
+    if pokemon_name == "Indeedee": pokemon_name = "indeedee-male"
+    if "Necrozma" in pokemon_name and pokemon_name.endswith("Wings"): pokemon_name = pokemon_name[:-6]
+    if "Necrozma" in pokemon_name and pokemon_name.endswith("Mane"): pokemon_name = pokemon_name[:-5]
+    if pokemon_name == "Giratina": pokemon_name = "giratina-altered"
+    if pokemon_name == "Mimikyu": pokemon_name = "mimikyu-disguised"
+    if pokemon_name == "Dudunsparce": pokemon_name = "dudunsparce-two-segment"
+    if pokemon_name.startswith("Tauros-Paldea"): pokemon_name += "-Breed"
+    if pokemon_name == "Lycanroc": pokemon_name = "lycanroc-midday"
+    if pokemon_name == "Toxtricity": pokemon_name = "toxtricity-low-key"
+    try:
+        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}")
+        data = response.json()
+    except:
+        print(pokemon_name)
+
+    # Base stats
+    stats = {stat["stat"]["name"]: stat["base_stat"] for stat in data["stats"]}
+    # Types
+    types = [t["type"]["name"] for t in data["types"]]
+    weight = data["weight"]
+    return stats, types, weight
+
+def get_all_mons(format="gen9vgc2025regg-1760"):
+    with open(os.path.join("data","raw","chaos",f"{format}.json"),"r") as file:
+        data = json.load(file)
+
+    # Get the full data dictionary
+    pokemon_stats = data["data"]
+
+    # Sort by 'usage' in descending order
+    sorted_pokemon = sorted(
+        pokemon_stats.items(),
+        key=lambda item: item[1].get("usage", 0),
+        reverse=True
+    )
+    # print(pokemon_stats)
+
+
+    # Print them out nicely
+    df = pd.DataFrame(columns=["name","types","weight","hp","atk","def","spatk","spdef","speed"])
+    for rank, (name, stats) in enumerate(sorted_pokemon, 1):
+        # print(f"{rank}. {name}: {stats['usage']:.2%}")
+        if name == "Palafin":
+            write_mon_data("palafin-zero", df)
+            name = "palafin-hero"
+        
+        df = write_mon_data(name, df)
+    # myFile = open(os.path.join("data","clean","info.txt"),"a")
+    df.to_csv(os.path.join("data","clean","dex.txt"))
+
+def write_mon_data(name, df):
+    # mon_dict = {"name","type1","type2","weight","hp","atk","def","spatk","spdef","speed"}
+    stats, types, weight = get_pokemon_base_info(name)
+    if len(types) > 1: types = [f"{types[0]}/{types[1]}"]
+    mon_data = [name, types.pop(), weight]
+    for stat in stats:
+        # print(f"{stat}:{stats[stat]}")
+        mon_data.append(stats[stat])
+    df.loc[len(df)] = mon_data
+    return df
+# info = get_pokemon_info("Infernape")
+get_all_mons()
+# stats, types, weight = get_pokemon_base_info("Urshifu-Rapid-Strike")
+# if len(types) > 1: types = [f"{types[0]}\{types[1]}"]
+# print(types.pop())
